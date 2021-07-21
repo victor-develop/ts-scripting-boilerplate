b1987acea54eb1c0d37a3e2a97bc1bf4f56feca5
Victor Zhou <victor_career@outlook.com>
2021-07-21 10:28:12 +0800

    :sparkles: Add example to decorate stdout

--- src/repl/transformStdOut.ts
+++ src/repl/transformStdOut.ts
@@ -1,7 +1,11 @@
 // import {default as safeStringify} from 'fast-safe-stringify'
 
+const formatJSONString = (x:unknown) => JSON.stringify(
+    JSON.parse((x as string)),
+    null,
+    2
+)
+
 export const transformStdOut = (stream: Highland.Stream<unknown>) => stream
 // This is an example, you can keep add multiple map() to transform the standard output
-.map(x => {
-    return x
-})
+.map(formatJSONString)
