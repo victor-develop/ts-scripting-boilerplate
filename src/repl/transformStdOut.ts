de1a1e3a55d52171a33701d790f6a689dc9e6250
Victor Xinghai Zhou Purple <victor.develop@outlook.com>
2021-08-09 01:42:27 +0800

    :sparkles: Render error in red

--- src/repl/transformStdOut.ts
+++ src/repl/transformStdOut.ts
@@ -1,23 +1,46 @@
 import {default as safeStringify} from 'fast-safe-stringify'
-const formatJSONString = (x:unknown) => safeStringify(
-    x as string,
-    null,
-    2
-)
+const prepareJsonString = (x:unknown) => {
+  return {
+    json: x,
+    string: safeStringify(
+      x as string,
+      null,
+      2
+  )
+  }
+}
+
+type RenderPipelineObj = {json: unknown, string: string}
+type RenderPipeline = (x: RenderPipelineObj) => RenderPipelineObj
 
 import * as chalk from 'chalk'
 import {EOL} from 'os'
-const addSplitLine = (x:string) => {
+const addSplitLine: RenderPipeline = ({json, string}) => {
   const [terminal_width] = process.stdout.getWindowSize()
   const line = chalk.blue.underline(Array(terminal_width).fill(' ').join(''))
-  return `${x}${EOL}${line}${EOL}${EOL}`
+  return {
+    json,
+    string: `${string}${EOL}${line}${EOL}${EOL}`
+  }
+}
+
+const redError: RenderPipeline = x => {
+  const string =  (x.json as unknown as {err?: unknown}).err?
+  chalk.red(x.string):
+  x.string
+  return {
+    ...x,
+    string
+  }
 }
 
 import { JSONString } from '../json/types'
 // This demonstrates how you can format the standard output
 export const transformStdOut = (stream: Highland.Stream<JSONString>):Highland.Stream<string> => stream
-// For standard output you just want to inspect a few samples
-.take(5)
 .map(JSON.parse)
-.map(formatJSONString)
+// From now on start the rendering piepline
+.map(prepareJsonString)
+.map(redError)
 .map(addSplitLine)
+// last function to render string
+.map(x => x.string)
