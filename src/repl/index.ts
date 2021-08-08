01c92d5ff34c54f226d852422b3a7613865a029e
Victor Xinghai Zhou Purple <victor.develop@outlook.com>
2021-08-09 01:44:39 +0800

    :sparkles: Setup socket io

--- src/repl/index.ts
+++ src/repl/index.ts
@@ -3,12 +3,16 @@ import * as faker from 'faker'
 import * as highland from 'highland'
 import { JSONString } from '../json/types';
 import { transformStdOut } from './transformStdOut';
+import { Server as SocketIoServer } from 'socket.io';
+import * as  Koa from 'koa';
+import * as KoaStatic from 'koa-static';
+import * as KoaMount from 'koa-mount';
+import * as http from 'http';
 
 const script_run_slug = faker.lorem.slug() + '_' + (new Date()).toISOString();
 const log_dir = './_log';
 
 import * as fs from 'fs'
-import { serveStatic } from './webOutputStream';
 function stream2File(s: Highland.Stream<JSONString>): void {
     (!fs.existsSync(log_dir)) && fs.mkdirSync(log_dir);
     s.pipe(
@@ -34,6 +38,13 @@ async function main() {
      * Start logging to the logger stream.
      */
     const logger = bunyan.createLogger({name: script_run_slug, stream: (logger_stream as unknown as NodeJS.WritableStream)})
+    process.on('unhandledRejection', error => {
+        logger.error(error)
+      });
+
+    process.on('exit', (code) => {
+        logger.info(`process exist with ${code}`)
+    })
 
     /**
      * Do whatever experiments
@@ -41,13 +52,26 @@ async function main() {
     logger.info('feels good')
     logger.info('experiments 567')
     
-    serveStatic(logger.child({mod: 'serveStatic'}))
+    const rootApp = new Koa();
+
+    const staticFileServer = new Koa()
+    staticFileServer.use(KoaStatic('src/repl/webOutputStream/staticAssets'));
+    rootApp.use(KoaMount('/app', staticFileServer))
+
+    const server = http.createServer(rootApp.callback())
+    const port = 3003
+    server.on('listening', () => {
+        logger.info(`listened on port ${port}`)
+    })
+    server.listen(port);
+
+    const io = new SocketIoServer(server);
+    io.on('connection', (socket) => {
+        logger.info(`a user connected: ${socket.id}`);
+    });
 
     logger.info('script run: finished!')
 
-    process.on('exit', (code) => {
-        logger.info(`process exist with ${code}`)
-    })
 }
 
 main()
--- src/repl/webOutputStream/index.ts
+++ src/repl/webOutputStream/index.ts
@@ -3,7 +3,7 @@ import { Http, HttpPipeline } from 'farrow-http'
 export const serveStatic = (logger): HttpPipeline  => {
     const http = Http()
     const port = 3003
-    http.serve('/', 'src/repl/webOutputStream/staticAssets')
+    http.serve('/app', 'src/repl/webOutputStream/staticAssets')
     http.listen(port, () => {
         logger.info(`open browser at port 3003!`)
     })
