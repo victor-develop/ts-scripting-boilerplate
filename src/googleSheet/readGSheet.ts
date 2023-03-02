e4f4855f182d0b27b458f0161f01e6ca4e80b558
Victor Zhou <victor_career@outlook.com>
2023-03-03 01:02:56 +0800

    :doc: Add example output as comment

--- src/googleSheet/readGSheet.ts
+++ src/googleSheet/readGSheet.ts
@@ -5,6 +5,7 @@ export async function readGSheet({logger}) {
     const doc = new GoogleSpreadsheet('1TXKbuP6n0XeKs4e9y-qoXlZpyXufXgNp0k3NSXMiFqY');
     await doc.useServiceAccountAuth(creds);
     await doc.loadInfo()
+    // example output: {"name":"quisquam-architecto-aliquid_2023-03-02T16:49:18.670Z","hostname":"Zhou.local","pid":40581,"level":30,"sheetObj":[{"Key":"My Key","v1":"v1","v2":"v2"}],"msg":"","time":"2023-03-02T16:49:19.816Z","v":0}
     logger.info(
         {
             sheetObj: (await doc.sheetsByTitle['main'].getRows())
